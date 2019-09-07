c49982ebdf9cb1c012d20b142f81c7f4c8faa190
oaflopean <oaflopean@gmail.com>
2019-09-07 06:17:05 +0000

    Sept 7

---
+++ flask/app.py
@@ -0,0 +1,283 @@
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+import requests
+import logging.config
+from caesarcipher import CaesarCipher
+import os
+
+from flask import g, session, jsonify, Markup
+from requests_oauthlib import OAuth2Session
+
+from flask import Flask, current_app, render_template_string
+from caesarcipher import CaesarCipher
+
+# Flask constructor takes the name of
+# current module (__name__) as argument.
+
+
+from flask import render_template, request, url_for, redirect, flash
+
+import ebooklib
+from pymongo import MongoClient
+from mongoengine import *
+import json
+import praw
+import requests
+import os
+import random
+from flask_wtf import Form
+from flask_migrate import Migrate
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from rake_nltk import Rake
+from wtforms.validators import DataRequired
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_login import current_user, login_user
+from werkzeug.urls import url_parse
+from psycopg2 import errors
+import praw
+
+# The route() function of the Flask class is a decorator,
+# which tells the application which URL should call
+# the associated function.
+
+app = Flask(__name__)
+
+login = LoginManager(app)
+login.login_view = 'login'
+app.config['SQLALCHEMY_DATABASE_URI']="postgresql://doadmin:t264wg0yfx9d6sf7@copy-com1234-do-user-4689509-0.db.ondigitalocean.com:25060/cp-admin?sslmode=require"
+app.config['STATIC_FOLDER']='static/'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+OAUTH2_CLIENT_ID = "619043497470590997"
+OAUTH2_CLIENT_SECRET = "0lvNMyaWMXrjEHWIk6KybP4cw9_SDx9t"
+OAUTH2_REDIRECT_URI = 'http://oaflopean.pythonanywhere.com/callback'
+
+API_BASE_URL = os.environ.get('API_BASE_URL', 'https://discordapp.com/api')
+AUTHORIZATION_BASE_URL = API_BASE_URL + '/oauth2/authorize'
+TOKEN_URL = API_BASE_URL + '/oauth2/token'
+app.config['SECRET_KEY'] = OAUTH2_CLIENT_SECRET
+from forms import SearchSub, LoginForm, RegistrationAppForm, PostForm, Titles, Chapters
+from models import User, Post, Bots, Result, Books,  RedditPost, Subreddits
+
+
+#@app.route('/<method>/<key>')
+def hello_world(method, key):
+    urls=[]
+    for url_list in app.url_map.iter_rules():
+        urls.append(url_list.rule)
+        for abc in range(26):
+            cipher = CaesarCipher(method,offset= abc).encoded
+            print(cipher + url_list.rule)
+            if cipher== url_list.rule.strip("/"):
+                print("yes")
+                method= cipher
+                key = CaesarCipher(key, offset=abc).encoded
+                print(key)
+                print(method)
+                return pod(method,key= key)
+            else:
+                continue
+    return str([a for a in urls])
+
+
+
+@app.route('/?key=<key>?titles=<yes>', methods=['GET', 'POST']) #allow both GET and POST requests
+@app.route('/?key=<key>', methods=['GET', 'POST']) #allow both GET and POST requests
+@app.route('/', methods=['GET', 'POST'])
+def push():
+    texts=[]
+    if request.method == 'POST':
+        reply=""
+        if request.args.get("key"):
+            key=request.args.get("key")
+
+            count=Books.query.filter_by(uri=key).count()
+            if count>0:
+                content=Books.query.filter_by(uri=key).first()
+                reply= content.description
+                return reply
+            else:
+                reddit = praw.Reddit(client_id='FCBZa-yDqRLNag', client_secret="ggD5MpCO7cQxbScgXaNmNydxPkk", password='AptCmx4$', user_agent='Copypasta', username="caesarnaples2")
+                for submission in reddit.subreddit(key).new():
+
+                    if request.args.get("yes"):
+                        reply=reply+" " + submission.title+" "
+                    else:
+                        reply=reply+" " + submission.selftext+" "
+                return reply
+    else:
+        form2 = Titles()
+        if form2.validate_on_submit():
+            book = Books()
+            book.title = form2.title.data
+            book.author = form2.author.data
+            book.description = form2.description.data
+            try:
+                book.username = current_user.username
+            except AttributeError:
+                book.username = "caesarnaples2"
+            s = "abcdefghijklmnopqrstuvwxyz"
+            passlen = 12
+            book.uri = "".join(random.sample(s, passlen))
+
+            kw = book.description
+            title = book.title
+            author = book.author
+            this_bot = Bots.query.filter_by(username="caesarnaples2").first()
+            try:
+                client_id = this_bot.client_id
+            except AttributeError:
+                return redirect("register/app")
+            secret = this_bot.secret
+            password = this_bot.password
+            username = this_bot.username
+            reddit = praw.Reddit(client_id=client_id,
+                                 client_secret=secret, password=password,
+                                 user_agent='Copypasta', username="caesarnaples2")
+
+            try:
+                reddit_url = reddit.subreddit('publishcopypasta').name
+
+                post = RedditPost(uri=book.uri, reddit_url=reddit_url, title=book.title, body=book.description,
+                                  username=book.username)
+                book.reddit_url = reddit_url
+                db.session.add(post)
+                db.session.commit()
+                db.session.add(book)
+                db.session.commit()
+            except KeyError:
+                print("error on db commit")
+            return redirect(url_for(pod()))
+        if request.args.get("key"):
+            key=request.args.get("key")
+            count=Books.query.filter_by(uri=key).count()
+            if count>0:
+                content=Books.query.filter_by(uri=key).first()
+                return render_template_string(str(content.description))
+            else:
+                url = 'https://www.reddit.com/r/' + key + '/new/.json?limit=500'
+
+                data = requests.get(url, headers={'user-agent': 'scraper by /u/ciwi'}).json()
+
+                for link in data['data']['children']:
+                    body = link['data']['selftext']
+                    texts.append(body.replace("\\","").replace("}","").replace("{",""))
+                newt="<br><br>".join(texts)
+                return render_template_string(Markup(newt))
+        else:
+            content = Books.query.filter_by().order_by(Books.id.desc()).all()
+            string_response =  "<html>{%include 'header.html'%}<body>{%include 'books.html'%}"
+            for box in content:
+                string_response = string_response +"<h3>" + box.title + "</h3>"
+                string_response = string_response +"<h4>"+ box.author + "</h4><br><big>Key= <a href='/?key="+ box.uri+"'>"+box.uri+ "</big></a><br>"
+                string_response = string_response + box.description.replace('\n', "<br>").replace("{","").replace("}","")
+            return render_template_string(string_response+"</html></body>", form2=form2)
+
+@app.route("/pod", methods=['GET', 'POST'])
+def pod():
+    form2 = Titles()
+
+    title = "Submit " + str(app)
+    username = "caesarnaples2"
+
+    if form2.validate_on_submit():
+        book = Books()
+        book.title = form2.title.data
+        book.author = form2.author.data
+        book.description = form2.description.data
+        try:
+            book.username = username
+        except AttributeError:
+            book.username = "caesarnaples2"
+        s = "abcdefghijklmnopqrstuvwxyz"
+        passlen = 6
+        book.uri = "".join(random.sample(s, passlen))
+        this_bot = Bots.query.filter_by(username="caesarnaples2").first()
+        try:
+            client_id = this_bot.client_id
+        except AttributeError:
+            return redirect("/")
+        secret = this_bot.secret
+        password = this_bot.password
+        username = this_bot.username
+        reddit = praw.Reddit(client_id=client_id,
+                             client_secret=secret, password=password,
+                             user_agent='Copypasta', username="caesarnaples2")
+
+        try:
+            reddit_url = reddit.subreddit('publishcopypasta').name
+        except praw.exceptions.APIException:
+            reddit_url = "No url"
+        post = RedditPost(uri=book.uri, reddit_url=reddit_url, title=book.title, body=book.description,
+                          username=book.username)
+        book.reddit_url = reddit_url
+        db.session.add(post)
+        db.session.commit()
+        db.session.add(book)
+        db.session.commit()
+        return redirect("/")
+
+if 'http://' in OAUTH2_REDIRECT_URI:
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = 'true'
+
+
+def token_updater(token):
+    session['oauth2_token'] = token
+
+
+def make_session(token=None, state=None, scope=None):
+    return OAuth2Session(
+        client_id=OAUTH2_CLIENT_ID,
+        token=token,
+        state=state,
+        scope=scope,
+        redirect_uri=OAUTH2_REDIRECT_URI,
+        auto_refresh_kwargs={
+            'client_id': OAUTH2_CLIENT_ID,
+            'client_secret': OAUTH2_CLIENT_SECRET,
+        },
+        auto_refresh_url=TOKEN_URL,
+        token_updater=token_updater)
+
+
+@app.route('/bot')
+def index():
+    scope = request.args.get(
+        'scope',
+        'identify email connections guilds guilds.join')
+    discord = make_session(scope=scope.split(' '))
+    authorization_url, state = discord.authorization_url(AUTHORIZATION_BASE_URL)
+    session['oauth2_state'] = state
+    return redirect(authorization_url)
+
+
+@app.route('/callback')
+def callback():
+    if request.values.get('error'):
+        return request.values['error']
+    discord = make_session(state=session.get('oauth2_state'))
+    token = discord.fetch_token(
+        TOKEN_URL,
+        client_secret=OAUTH2_CLIENT_SECRET,
+        authorization_response=request.url)
+    session['oauth2_token'] = token
+    return redirect(url_for('.me'))
+
+
+@app.route('/me')
+def me():
+    discord = make_session(token=session.get('oauth2_token'))
+    user = discord.get(API_BASE_URL + '/users/@me').json()
+    guilds = discord.get(API_BASE_URL + '/users/@me/guilds').json()
+    connections = discord.get(API_BASE_URL + '/users/@me/connections').json()
+    return jsonify(user=user, guilds=guilds, connections=connections)
+
+
+if __name__ == '__main__':
+    # run() method of Flask class runs the application
+    # on the local development server.
+    app.run()
---
+++ flask/models.py
@@ -0,0 +1,106 @@
+from sqlalchemy.dialects.postgresql import JSON
+from datetime import datetime
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from app import db
+user={}
+user["username"]="caesarnaples2"
+
+class Result(db.Model):
+    __tablename__ = 'results'
+    id = db.Column(db.Integer, primary_key=True)
+    kw = db.Column(db.String())
+    sub = db.Column(db.String())
+    username = db.Column(db.String(), db.ForeignKey('user.username'))
+
+    def __repr__(self):
+        return '<id {}>'.format(self.kw)
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class Bots(db.Model):
+    __tablename__ = 'bots'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64),  db.ForeignKey('user.username'))
+    app_name= db.Column(db.String(128))
+    client_id= db.Column(db.String(128))
+    secret= db.Column(db.String(128))
+    password= db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password=password
+
+    def check_password(self, password):
+        return self.password
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
+
+class Books(db.Model):
+    __tablename__='books'
+    id  = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(300))
+    author=db.Column(db.String(300))
+    username = db.Column(db.String(64), db.ForeignKey('user.username'))
+    description = db.Column(db.String())
+    uri=db.Column(db.String(), db.ForeignKey('redditpost.uri'))
+
+    def __repr__(self):
+        try:
+            string='Book: {}>'.format(self.title+" by "+self.author+"\n"+self.description)
+        except TypeError:
+            string='Book: {}>'.format(self.title)
+        return string
+
+
+class RedditPost(db.Model):
+    __tablename__='redditpost'
+    id  = db.Column(db.Integer(), primary_key=True)
+    uri = db.Column(db.String(), unique=True)
+    reddit_url=db.Column(db.String())
+    title=db.Column(db.String())
+    body = db.Column(db.String())
+    integer=db.Column(db.Integer())
+
+    username=db.Column(db.String(64), db.ForeignKey("user.username"))
+
+    def __repr__(self):
+        try:
+            string='POST: {}'.format(self.title + " <a href=\"admin?uri="+self.uri+"\">"+self.body+"</a>")
+            return string
+        except TypeError:
+            string='POST: {}'.format(self.id)
+            return string
+
+class Subreddits(db.Model):
+    __tablename__='subreddits'
+    id  = db.Column(db.Integer(), primary_key=True)
+    sub= db.Column(db.String(), unique=True)
+    def __repr__(self):
+        string='Sub: {}'.format(self.sub)
+        return string
